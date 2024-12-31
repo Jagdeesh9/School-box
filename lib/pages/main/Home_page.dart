fec2cffd5d2dbf6e9d53ffbb15e6119d83402df5
unknown <jagdeeshbhandari05@gmail.com>
2024-12-31 10:15:53 +0530

    starting commit

--- lib/pages/main/Home_page.dart
+++ lib/pages/main/Home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/rendering.dart';
 import 'package:school_box/colors.dart';
 import 'package:school_box/common/utils/helpers.dart';
 import 'package:school_box/constants/colors.dart';
+import 'package:school_box/pages/Screens/subscription_detail.dart';
 import 'package:school_box/widgets/carousel.dart';
 
 class HomePage extends StatefulWidget {
@@ -29,7 +30,7 @@ class _HomePageState extends State<HomePage> {
         title: const Row(
           children: [
             CircleAvatar(
-              backgroundImage: AssetImage('assets/images/Otp.png'),
+              backgroundImage: AssetImage('assets/images/profile.png'),
             ),
 
             SizedBox(width: 8), // Space between image and text
@@ -132,109 +133,195 @@ class Foodrow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(color: getBorderColor(context)),
+    return InkWell(
+      onTap: () {
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (context) => const Subscription_Detail(),
+        //   ),
+        // );
+
+        showBottomSheet(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
           ),
-        ),
-        child: Row(
-          children: [
-            //image container
-            Container(
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border(
-                  right: BorderSide(
-                    color: getBorderColor(context),
-                  ),
+          context: context,
+          builder: (context) {
+            return SizedBox(
+              height: 300,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 150, // Adjust height as needed
+                      decoration: BoxDecoration(
+                        color: AppColors.black,
+                        borderRadius:
+                            BorderRadius.circular(12.0), // Curved corners
+                        boxShadow: [
+                          BoxShadow(
+                            color:
+                                Colors.black.withOpacity(0.2), // Shadow color
+                            blurRadius: 8, // Blur intensity
+                            offset: const Offset(0, 4), // Shadow position
+                          ),
+                        ],
+                        border: Border.all(
+                          color: getBorderColor(context), // Custom border color
+                          width: 1.5,
+                        ),
+                      ),
+                      child: ClipRRect(
+                        borderRadius:
+                            BorderRadius.circular(12.0), // Match the curve
+                        child: Image.asset(
+                          'assets/images/slide-1.jpg',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      "Daily mix veg food subscription",
+                      style: Theme.of(context).textTheme.titleLarge,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      'Mix Veg, Chapati, Mithai, 4 Bananas, Mix Veg, Chapati, Mithai, 4 Bananas',
+                      style: Theme.of(context).textTheme.labelLarge,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    const Subscription_Detail(),
+                              ),
+                            );
+                          },
+                          child: Text('View Details')),
+                    )
+                  ],
                 ),
               ),
-              height: 80,
-              width: 80,
-              child: Image.asset(
-                'assets/images/slide-1.jpg',
-                fit: BoxFit.cover,
-              ),
+            );
+          },
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(color: getBorderColor(context)),
             ),
-
-            const SizedBox(
-              width: 10,
-            ),
-
-            //food text
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    overflow: TextOverflow.ellipsis,
-                    "Daily mix veg food subscription",
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    'Mix Veg, Chapati, Mithai, 4 Bananas,Mix Veg, Chapati, Mithai, 4 Bananas ',
-                    style: Theme.of(context).textTheme.labelLarge,
-                  )
-                ],
-              ),
-            ),
-
-            //food price
-            const SizedBox(
-              width: 10,
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              height: 80,
-              width: 80,
-              decoration: BoxDecoration(
-                border: Border(
-                  left: BorderSide(
-                    color: getBorderColor(context),
-                  ),
-                ),
-              ),
-              child: Column(
-                children: [
-                  //green icons
-                  Container(
-                    height: 20,
-                    width: 20,
-                    child: const Center(child: Text('₹')),
-                    decoration: BoxDecoration(
-                      color: AppColors.secondaryColor,
-                      borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            children: [
+              // Image container
+              Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border(
+                    right: BorderSide(
+                      color: getBorderColor(context),
                     ),
                   ),
-
-                  const SizedBox(
-                    height: 2,
-                  ),
-
-                  const Text(
-                    '2000',
-                    style: TextStyle(color: AppColors.warning, fontSize: 12),
-                  ),
-
-                  const Text(
-                    '2500',
-                    style: TextStyle(
-                        fontSize: 12,
-                        decorationColor: AppColors.darkGrey,
-                        color: AppColors.darkGrey,
-                        decoration: TextDecoration.lineThrough),
-                  )
-                ],
+                ),
+                height: 80,
+                width: 80,
+                child: Image.asset(
+                  'assets/images/slide-1.jpg',
+                  fit: BoxFit.cover,
+                ),
               ),
-            )
-          ],
+
+              const SizedBox(
+                width: 10,
+              ),
+
+              // Food text
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      overflow: TextOverflow.ellipsis,
+                      "Daily mix veg food subscription",
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Mix Veg, Chapati, Mithai, 4 Bananas, Mix Veg, Chapati, Mithai, 4 Bananas ',
+                      style: Theme.of(context).textTheme.labelLarge,
+                    )
+                  ],
+                ),
+              ),
+
+              // Food price
+              const SizedBox(
+                width: 10,
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  border: Border(
+                    left: BorderSide(
+                      color: getBorderColor(context),
+                    ),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    // Green icon
+                    Container(
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                        color: AppColors.secondaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(child: Text('₹')),
+                    ),
+
+                    const SizedBox(
+                      height: 2,
+                    ),
+
+                    const Text(
+                      '2000',
+                      style: TextStyle(color: AppColors.warning, fontSize: 12),
+                    ),
+
+                    const Text(
+                      '2500',
+                      style: TextStyle(
+                          fontSize: 12,
+                          decorationColor: AppColors.darkGrey,
+                          color: AppColors.darkGrey,
+                          decoration: TextDecoration.lineThrough),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
